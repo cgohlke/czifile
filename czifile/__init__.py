fd6d48516cf3bb67e0f92d7de925e2058e4b6298
Christoph Gohlke <cgohlke@uci.edu>
2018-10-19 23:13:17 -0700

    Update czifile/__init__.py

--- czifile/__init__.py
+++ czifile/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # czifile/__init__.py
 
-from .czifile import __doc__, __all__, __version__  # noqa
-from .czifile import *  # noqa
+from .czifile import __doc__, __all__, __version__
+from .czifile import *
