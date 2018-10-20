a5c5ec543e85d972157727736478a1e0d50b9c1f
Christoph Gohlke <cgohlke@uci.edu>
2018-10-19 23:29:18 -0700

    Add zisraw.py

---
+++ zisraw.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# zisraw.py
+
+"""Proxy module for the czifile package."""
+
+from czifile.czifile import __doc__, __all__, __version__  # noqa
+from czifile.czifile import czi2tif  # noqa
+from czifile.czifile import *  # noqa
