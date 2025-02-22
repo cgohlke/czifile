99a6b493be166ecd6dc57544237daea4d194f421
Christoph Gohlke <cgohlke@cgohlke.com>
2025-02-22 10:32:35 -0800

    Update czifile/__init__.py

--- czifile/__init__.py
+++ czifile/__init__.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 # czifile/__init__.py
 
-from .czifile import __doc__, __all__, __version__
 from .czifile import *
+from .czifile import __all__, __doc__, __version__
+
+
+def _set_module():
+    """Set __module__ attribute for all public objects."""
+    globs = globals()
+    module = globs['__name__']
+    for item in __all__:
+        obj = globs[item]
+        if hasattr(obj, '__module__'):
+            obj.__module__ = module
+
+
+_set_module()
