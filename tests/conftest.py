dca4e2e6298ca5957fb38b779f1f02f6de75469f
Christoph Gohlke <cgohlke@cgohlke.com>
2026-03-12 18:37:14 -0700

    Update tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,41 @@
-# -*- coding: utf-8 -*-
 # czifile/tests/conftest.py
 
-collect_ignore = ['_tmp', 'data']
+import os
+import sys
+
+if os.environ.get('VSCODE_CWD'):
+    # work around pytest not using PYTHONPATH in VSCode
+    sys.path.insert(
+        0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    )
+
+if os.environ.get('SKIP_CODECS', None):
+    sys.modules['imagecodecs'] = None
 
 
 def pytest_report_header(config):
     try:
-        import czifile
-        import imagecodecs
-        return 'versions: czifile-%s, imagecodecs-%s, numpy-%s' % (
-            czifile.__version__, imagecodecs.__version__, numpy.__version__)
+        from numpy import __version__ as numpy
+        from test_czifile import config
+        from tifffile import __version__ as tifffile
+
+        from czifile import __version__ as czifile
+
+        try:
+            from imagecodecs import __version__ as imagecodecs
+        except ImportError:
+            imagecodecs = 'N/A'
+        return (
+            f'versions: czifile-{czifile}, '
+            f'tifffile-{tifffile}, '
+            f'imagecodecs-{imagecodecs}, '
+            f'numpy-{numpy}\n'
+            f'test config: {config()}'
+        )
     except Exception:
         pass
+
+
+collect_ignore = ['_tmp', 'data']
+
+# mypy: ignore-errors
