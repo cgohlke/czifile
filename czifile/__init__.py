30674ef5f6367cb5cf79adae8e030e3c477a8f56
Christoph Gohlke <cgohlke@cgohlke.com>
2026-03-14 13:50:50 -0700

    Update czifile/__init__.py

--- czifile/__init__.py
+++ czifile/__init__.py
@@ -7,3 +7,9 @@ from .czifile import __all__, __doc__, __version__
 
 __version__ = __version__
 """Czifile version string."""
+
+CONVERT_PIXELTYPE = CONVERT_PIXELTYPE
+"""Map of (source, target) CziPixelType pairs to converter functions.
+
+:meta hide-value:
+"""
